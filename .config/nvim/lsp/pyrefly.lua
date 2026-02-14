1531a8e03c82a87603201bf2bedf7052d0dfe610
Danny Bozbay <107803920+dbozbay@users.noreply.github.com>
2026-02-14 20:34:23 +0000

    reconfigure pyrefly

--- .config/nvim/lsp/pyrefly.lua
+++ .config/nvim/lsp/pyrefly.lua
@@ -6,6 +6,13 @@ return {
 		"pyproject.toml",
 		".git",
 	},
+	settings = {
+		python = {
+			pyrefly = {
+				displayTypeError = "force-on",
+			},
+		},
+	},
 	filetypes = { "python" },
 	on_exit = function(code, _, _)
 		vim.notify("Closing Pyrefly LSP exited with code: " .. code, vim.log.levels.INFO)
